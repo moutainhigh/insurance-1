9a6d4518acc5b8a09cc8b4c78003fcb16b04046a
jinningxia <jinningxia@souche.com>
2018-05-25 15:38:51 +0800

    去掉不用的项目

--- yundian.uploader/src/main/java/com/yundian/file/service/UploadService.java
+++
@@ -1,9 +0,0 @@
-package com.yundian.file.service;
-
-import javax.servlet.http.HttpServletRequest;
-
-public interface UploadService {
-	public String uploadfile(HttpServletRequest request);
-
-	public String uploadFileForBase64(String filecontent, String fileBasePath, String newdir, String extname);
-}
